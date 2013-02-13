75f8fa26aa5dd5ca3c2151f9b58e2afda5f3553a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-02-13 12:44:40 +0100

    ansible's hosts file moved to separate directory

--- ansible-host.sh
+++ ansible-host.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ANSIBLE_ASK_SUDO_PASS=${ask_sudo_pass} \
-ANSIBLE_HOSTS=files/etc_ansible_hosts \
+ANSIBLE_HOSTS=ansible/hosts \
 ANSIBLE_REMOTE_PORT=${remote_port:-22} \
 ANSIBLE_REMOTE_USER=${remote_user:-root} \
 ANSIBLE_TRANSPORT=ssh \
--- ansible-playbook-shrot.sh
+++ ansible-playbook-shrot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ANSIBLE_HOSTS=files/etc_ansible_hosts \
+ANSIBLE_HOSTS=ansible/hosts \
 ANSIBLE_REMOTE_PORT=${ssh_port:-2220} \
 ANSIBLE_PRIVATE_KEY_FILE=keys/id_rsa \
 ANSIBLE_REMOTE_TEMP=/tmp/ansible-root \
