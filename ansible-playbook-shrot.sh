983d17b6b6c18644e0daa46b3bfc01a69d3dd31c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-02-13 17:23:20 +0100

    Export some variables to ansible-playbook command

--- ansible-playbook-shrot.sh
+++ ansible-playbook-shrot.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+. $(dirname $0)/common.inc
+
+read_profiles "$@"
+
+vars="$(eval echo $(set|egrep '^(vendor|suite|arch|role|shrot|shrot_home|ssh_port)='))"
+
 ANSIBLE_HOSTS=ansible/hosts \
 ANSIBLE_REMOTE_PORT=${ssh_port:-2220} \
 ANSIBLE_PRIVATE_KEY_FILE=keys/id_rsa \
 ANSIBLE_REMOTE_TEMP=/tmp/ansible-root \
 ANSIBLE_REMOTE_USER=root \
 ANSIBLE_TRANSPORT=paramiko \
-ansible-playbook "$@"
+ansible-playbook -e "$vars" "$@"
