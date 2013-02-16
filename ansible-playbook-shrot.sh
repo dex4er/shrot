eef104b3d19dff139711236ab72350db56e0dd6e
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-02-16 15:08:03 +0100

    .gitignore instead .placeholders, create ansible/hosts if missing

--- ansible-playbook-shrot.sh
+++ ansible-playbook-shrot.sh
@@ -8,6 +8,8 @@ test -f "$playbook" || die "playbook $playbook not found"
 
 vars="$(eval echo $(set|egrep '^(vendor|suite|arch|role|shrot|shrot_home|ssh_port)='))"
 
+test -f ansible/hosts || echo localhost > ansible/hosts
+
 ANSIBLE_HOSTS=ansible/hosts \
 ANSIBLE_REMOTE_PORT=${ssh_port:-2220} \
 ANSIBLE_PRIVATE_KEY_FILE=keys/id_rsa \
