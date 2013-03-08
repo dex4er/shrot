898ced4e35ed8f3612095123704adb9cc55e397e
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-03-08 21:18:43 +0100

    Generate public key and ascii-art fingerprint if private key already exists

--- generate-keys.sh
+++ generate-keys.sh
@@ -5,18 +5,18 @@
 # (c) 2013 Piotr Roszatycki <piotr.roszatycki@gmail.com>
 
 create_key() {
-    msg="$1"
-    shift
-    comment="$1"
-    shift
-    file="$1"
-    shift
+    msg="$1"; shift
+    comment="$1"; shift
+    file="$1"; shift
 
     if [ ! -f "$file" ]; then
         echo -n $msg
         ssh-keygen -q -f "$file" -N '' -C "$comment" "$@"
         ssh-keygen -v -f "$file" -l > "$file.asc"
         echo
+    else
+        echo `ssh-keygen -y -f "$file" -C "$comment"` "$comment" > "$file.pub"
+        ssh-keygen -l -v -f "$file" > "$file.asc"
     fi
 }
 
