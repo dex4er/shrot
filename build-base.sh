7bc30c4f502edf31dcb58fb1b7be4dd8723502aa
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-03-10 21:43:49 +0100

    Fixed build scripts when profile sets host

--- build-base.sh
+++ build-base.sh
@@ -50,11 +50,11 @@ install -m 0600 -o root -g root keys/id_rsa.pub $target/root/.ssh/authorized_key
 
 run /etc/init.d/ssh start "-p$ssh_port -oAuthorizedKeysFile=%h/.ssh/authorized_keys_ansible"
 
-./ansible-playbook-shrot.sh "$@" host=localhost playbook=playbooks/ping.yml || error "playbook for ping failed"
+./ansible-playbook-shrot.sh host=localhost playbook=playbooks/ping.yml "$@" || error "playbook for ping failed"
 
-./ansible-playbook-shrot.sh "$@" host=localhost || error "playbook $playbook failed"
+./ansible-playbook-shrot.sh host=localhost "$@" || error "playbook $playbook failed"
 
-./ansible-playbook-shrot.sh "$@" host=localhost playbook=playbooks/clean.yml || error "playbook for clean failed"
+./ansible-playbook-shrot.sh host=localhost playbook=playbooks/clean.yml "$@" || error "playbook for clean failed"
 
 test -x $target/etc/init.d/rc.chroot && run /etc/init.d/rc.chroot stop
 run /etc/init.d/ssh stop
