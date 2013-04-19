72f976874bae09a4d4a125898d191fe89c7da150
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-04-19 20:55:14 +0200

    Default port is 0 (None). Support $ansible_debug variable.

--- ansible-playbook-host.sh
+++ ansible-playbook-host.sh
@@ -10,10 +10,16 @@ vars="$(echo $(export | sed -e 's/^declare -x //' -e 's/^export //' | egrep '^[a
 
 grep -qs "^$host$" ansible/hosts || echo $host >> ansible/hosts
 
+case "$ansible_debug" in
+    1) ansible_debug="-v";;
+    2) ansible_debug="-vv";;
+    3) ansible_debug="-vvv";;
+fi
+
 ANSIBLE_ASK_SUDO_PASS=${ask_sudo_pass:-} \
 ANSIBLE_HOSTS=ansible/hosts \
-ANSIBLE_REMOTE_PORT=${remote_port:-22} \
+ANSIBLE_REMOTE_PORT=${remote_port:-None} \
 ANSIBLE_REMOTE_TEMP=/tmp/ansible-root \
 ANSIBLE_REMOTE_USER=${remote_user:-root} \
 ANSIBLE_TRANSPORT=ssh \
-ansible-playbook -l $host -e "$vars" "$playbook" $ansible_playbook_args
+ansible-playbook -l $host -e "$vars" "$playbook" $ansible_debug $ansible_playbook_args
