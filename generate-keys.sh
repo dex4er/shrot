be7dafff357a1dbdef5ea0040127795cb1141f1d
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-07-09 14:12:03 +0200

    Generate just client keys without server keys

--- generate-keys.sh
+++ generate-keys.sh
@@ -26,15 +26,3 @@ mkdir -p keys
 create_key "Creating SSH2 RSA client key; this may take some time ..." \
     ansible@shrot \
     keys/id_rsa -t rsa
-
-# Server keys
-create_key "Creating SSH2 RSA server key; this may take some time ..." \
-    root@shrot \
-    keys/ssh_host_rsa_key -t rsa
-create_key "Creating SSH2 DSA server key; this may take some time ..." \
-    root@shrot \
-    keys/ssh_host_dsa_key -t dsa
-create_key "Creating SSH2 ECDSA server key; this may take some time ..." \
-    root@shrot \
-    keys/ssh_host_ecdsa_key -t ecdsa
-
